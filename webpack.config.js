00d51401a56cf5547b47248fa46cb56bff4357a4
johnrobertmcc <john.robert.mcc@gmail.com>
2020-11-22 21:04:41 -0500

    sdfsdfsfsfsdfsdfs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: __dirname,
@@ -22,44 +21,9 @@ module.exports = {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
-            },
-            {
-            test: /\.css$/,
-            use: ExtractTextWebpackPlugin.extract({ 
-                // 2. Use ExtractTextWebpackPlugin
-                // When not extracting, what kind of configuration is used to process css
-                // fallback refers to what loader is used to compile the code after compilation
-                fallback: {
-                    loader: 'style-loader',
-                    options: {
-                        //This attribute is generally used in style-loader,
-                        // refers to the unified merge of the code of multiple css files into a style tag
-                        singleton: true
-                    }
-                },
-                use: [ 
-                    // When extracting, continue to process in the following way
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            //Whether to enable the compression function
-                            minimize: true
-                        }
-                    }
-                ]
-            })
-        }
+            }
         ]
     },
-     plugins: [
-        new ExtractTextWebpackPlugin({ 
-            //3. Configure properties in plugins
-            filename: '[name].min.css', // Configure the extracted CSS name
-            //true means that all css will be extracted, false will only extract the initialization, the default is false
-            allChunks:false,
-            //
-            
-        })
-    ],
     devtool: 'source-map'
 };
+
